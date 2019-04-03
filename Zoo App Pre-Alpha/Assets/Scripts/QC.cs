40e0d9de5bb356b12a6c2c9b65db61a201984133
Trent Jones <zackattackqwert5@gmail.com>
2019-04-03 11:01:42 -0500

    Implemented Question/Answer System!
    Fully customizable questions can be written in the scripts. Answers can be chosen, and answer texts and question texts can be set. Correct answers send to the "correct" slide and incorrect choices send to the "incorrect" slide.

--- Zoo App Pre-Alpha/Assets/Scripts/QC.cs
+++ Zoo App Pre-Alpha/Assets/Scripts/QC.cs
@@ -7,9 +7,8 @@ using UnityEngine.UI;
 public class QC : MonoBehaviour
 {
     //public static int currentLocation { get; private set; }
-    public int Question = 0;//currentLocation/*value from AR Scanner*/ - 1 * 5; //Pick starting question
+    public int Question = 0;//currentLocation/*value from AR Scanner*/ - 1 * 5; //Pick starting question based on where the user is
     public Text question;
-    public int correctanswer;
 
     // Start is called before the first frame update
     void Start()
@@ -20,26 +19,24 @@ public class QC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Chooses text to display based on the question number
         if (Question == 0)
         {
             question.text = "Sample Question 0, Ans A";
-            correctanswer = 0;
         }
         if (Question == 1)
         {
             question.text = "Sample Question 1, Ans B";
-            correctanswer = 1;
         }
         if (Question == 2)
         {
             question.text = "Sample Question 2, Ans C";
-            correctanswer = 2;
         }
         if (Question == 3)
         {
             question.text = "Sample Question 3, Ans D";
-            correctanswer = 3;
         }
+        //Debugging tool, moves directly to next question when space is pressed
         if(Input.GetKeyDown(KeyCode.Space))
         {
             Question = Question + 1;
