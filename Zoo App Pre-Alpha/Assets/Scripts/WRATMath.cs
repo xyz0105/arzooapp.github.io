f3c800d277d685a8015691b13c85923785afd549
Trent Jones <zackattackqwert5@gmail.com>
2019-04-10 13:56:42 -0500

    Loop Created
    The AccuracyMath has been changed over to the more complete and accurate RAWTMath, along with fixing the Right, Accuracy, Wrong, and Total math. RAWT works. Additionally, wired a couple of butons properly, and the app is now 100% navigable (minus some questions, since there's only one AR Target so far).

---
+++ Zoo App Pre-Alpha/Assets/Scripts/WRATMath.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WRATMath : MonoBehaviour
+{
+    public Text accuracy;
+    public Text wrongs;
+    public Text rights;
+    public Text total;
+    public int AccuracyScore;
+    public int Total;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        accuracy.text = "";
+        wrongs.text = "";
+        rights.text = "";
+        total.text = "";
+        //Determine the user's accuracy as their # of correct answers, over their total number of answers, represented as a percentage.
+        Total = PlayerPrefs.GetInt("Rights") + PlayerPrefs.GetInt("Wrongs");
+        AccuracyScore = PlayerPrefs.GetInt("Rights") * 100 / Total;
+        accuracy.text = AccuracyScore + "%";
+        wrongs.text = PlayerPrefs.GetInt("Wrongs") + " Wrongs";
+        rights.text = PlayerPrefs.GetInt("Rights") + " Rights";
+        total.text = "Total: " + Total;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
