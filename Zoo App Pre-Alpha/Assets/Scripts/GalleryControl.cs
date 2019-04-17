052ba58d3e583ad0a7ea5620adc86594735be4a7
Trent Jones <zackattackqwert5@gmail.com>
2019-04-17 10:08:00 -0500

    Unlocking System
    Answering questions now unlocks the silhouettes on the hub, and clicking unlocked silhouettes sends you to the ARScene with a PlayerPrefs "ModelNumber" to project. Also, swapped Accuracy math to a more ambiguous Score math.

---
+++ Zoo App Pre-Alpha/Assets/Scripts/GalleryControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GalleryControl : MonoBehaviour
+{
+    //Initialize objects
+    public GameObject RhinoButton;
+    public GameObject PandaButton;
+    public GameObject LeopardButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Store actual objects inside the initialized variable
+        RhinoButton = GameObject.Find("RhinoButton");
+        PandaButton = GameObject.Find("PandaButton");
+        LeopardButton = GameObject.Find("LeopardButton");
+
+        //Set objects to active or inactive based on completion
+        if (PlayerPrefs.GetInt("Unlocked1") == 0)
+        {
+            RhinoButton.SetActive(false);
+        }
+        else
+        {
+            RhinoButton.SetActive(true);
+        }
+        if (PlayerPrefs.GetInt("Unlocked2") == 0)
+        {
+            PandaButton.SetActive(false);
+        }
+        else
+        {
+            PandaButton.SetActive(true);
+        }
+        if (PlayerPrefs.GetInt("Unlocked3") == 0)
+        {
+            LeopardButton.SetActive(false);
+        }
+        else
+        {
+            LeopardButton.SetActive(true);
+        }
+    }
+
+    public void ChooseProjection(int modelnumber)
+    {
+        PlayerPrefs.SetInt("ModelNumber", modelnumber);
+    }
+}
--- Zoo App Pre-Alpha/Assets/Scripts/WRATMath.cs
+++ Zoo App Pre-Alpha/Assets/Scripts/WRATMath.cs
@@ -5,27 +5,23 @@ using UnityEngine.UI;
 
 public class WRATMath : MonoBehaviour
 {
-    public Text accuracy;
+    public Text score;
     public Text wrongs;
     public Text rights;
-    public Text total;
-    public int AccuracyScore;
-    public int Total;
+    public int Score;
 
     // Start is called before the first frame update
     void Start()
     {
-        accuracy.text = "";
+        score.text = "";
         wrongs.text = "";
         rights.text = "";
-        total.text = "";
-        //Determine the user's accuracy as their # of correct answers, over their total number of answers, represented as a percentage.
-        Total = PlayerPrefs.GetInt("Rights") + PlayerPrefs.GetInt("Wrongs");
-        AccuracyScore = PlayerPrefs.GetInt("Rights") * 100 / Total;
-        accuracy.text = AccuracyScore + "%";
+        Score = PlayerPrefs.GetInt("Rights") * 100 + PlayerPrefs.GetInt("Wrongs") * -25;
+        //Displays score
+        score.text = "Score: " + Score;
+        //Debugging values, should be hidden on actual launch of app
         wrongs.text = PlayerPrefs.GetInt("Wrongs") + " Wrongs";
         rights.text = PlayerPrefs.GetInt("Rights") + " Rights";
-        total.text = "Total: " + Total;
     }
 
     // Update is called once per frame
