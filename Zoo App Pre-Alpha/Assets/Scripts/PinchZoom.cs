cf622f0d3259f9be2450463917093c0439386506
Garrett-Scott <47392469+Garrett-Scott@users.noreply.github.com>
2019-05-28 08:27:04 -0500

    added distance detector
    added a debug distance detector to determine the distance between two fingers. will be used for pinch zoom.

---
+++ Zoo App Pre-Alpha/Assets/Scripts/PinchZoom.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PinchZoom : MonoBehaviour {
+    public GameObject textbox;
+    private void Update()
+    {
+
+
+        if (Input.touchCount >= 2)
+        {
+            Vector2 touch0, touch1;
+            float distance;
+            touch0 = Input.GetTouch(0).position;
+            touch1 = Input.GetTouch(1).position;
+            distance = Vector2.Distance(touch0, touch1);
+
+            string s = string.Format("{0:G}", distance);
+            Text debugText = textbox.GetComponent<Text>();
+            debugText.text = s;
+            
+
+            
+                
+        }
+        else { Text debugText = textbox.GetComponent<Text>();
+            debugText.text = "2 inputs not detected";
+        }
+    }
+}
