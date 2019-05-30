e5e51c080d18095036fe97cadf80d998aefd6b6a
Garrett-Scott <47392469+Garrett-Scott@users.noreply.github.com>
2019-05-30 13:40:28 -0500

    Map Update
    map can be panned and zoomed in mobile, all animal locations have been programmed in

--- Zoo App Pre-Alpha/Assets/Scripts/GoHere.cs
+++ Zoo App Pre-Alpha/Assets/Scripts/GoHere.cs
@@ -8,18 +8,48 @@ public class GoHere : MonoBehaviour
 
     Vector3 PlayerLocation;
     public int location = 2;
-    private int currentLocation;
+    public int currentLocation;
     public int locationLimit;
-    
+
 
     /*
         Location Table:
        Integer "location code" for location, name of location, and Coordinates
 
        0 Offscreen
-       1 Red Panda (-192, -29)
-       2 Clouded Leopard (-180, -37)
-       3 Rhino (138, 132)
+       1 Hyacinth Macaw (5.26f, -1.27f, -0.0001f)
+       2 Stanley Crane (5.26f, -0.82f, -0.0001f)
+       3 White Cheeked Gibbon (3.45f, -0.47f, -0.0001f)
+       4 Red River Hog (2.81f, 2.01f, -0.0001f)
+       5 Southern White Rhino (2.04f, 2.5f, -0.0001f)
+       6 Giraffe (0.56f, 3.2f, -0.0001f)
+       7 Meerkat (2.16f, -0.13f, -0.0001f)
+       8 Saddle-Billed Stork (2.217f, -0.744f, -0.0001f)
+       9 Cassowary (0.439f, -0.156f, -0.0001f)
+       10 Kangaroo (1.131f, 0.383f, -0.0001f)
+       11 Eland (0.42f, 1.629f, -0.0001f)
+       12 Bontebok (0.42f, 1.629f, -0.0001f)
+       13 Gray Crowned Crane (0.42f, 1.629f, -0.0001f)
+       14 Otrich (0.42f, 1.629f, -0.0001f)
+       15 Springbok (0.42f, 1.629f, -0.0001f)
+       16 Zebra (0.42f, 1.629f, -0.0001f)
+       17 Sulcata Tortoise (-0.298f, 2.036f, -0.0001f)
+       18 Sumatran Tiger (-1.816f, 2.571f, -0.0001f)
+       19 Red-Footed Tortoise (-0.535f, 1.026f, -0.0001f)
+       20 Southern Pudu (-0.688f, 1.17f, -0.0001f)
+       21 Andean Bear (-1.495f, 1.107f, -0.0001f)
+       22 Guinea Pig (-1.014f, 0.35f, -0.0001f)
+       23 Rhino Hornbill (-1.837f, -0.266f, -0.0001f)
+       24 Red Panda (-2.598f, -0.958f, -0.0001f)
+       25 Clouded Leopard (-2.385f, -1.04f, -0.0001f)
+       26 Ring-Tailed Lemur (-2.61f, -0.052f, -0.0001f)
+       27 Yellow-Backed Duiker (-3.51f, -0.926, -0.0001f)
+       28 Red-Ruffed Lemur (-3.152f, 0.367f, -0.0001f)
+       29 Cougar (-4.451f, 0.572f, -0.0001f)
+       30 Mexican Spider Monkey (-3.869f, -1.614f, -0.0001f)
+       31 Cape Porcupine (-2.51f, -1.696f, -0.0001f)
+       32 Baird's Tapir/Crested Screamer (-1.256f, -2.209f, -0.0001f)
+       33 Caribbean Flamingo (-0.274f, -0.097f, -0.0001f)
 
         */
     // The target next location selects 
@@ -74,7 +104,76 @@ public class GoHere : MonoBehaviour
     void MoveSign(int locationCode)
     {
      
-            Vector3[] xcoordinates = new[] { new Vector3(1000.0f, 0.0f, 0.0f), new Vector3(-192.0f, -29.0f, 0.0f), new Vector3(-134.0f, -46.0f, 0.0f), new Vector3(138.0f, 132.0f, 0.0f) };
+            Vector3[] xcoordinates = new[] {
+                new Vector3(1000.0f, 0.0f, -1.0f), 
+                new Vector3
+               (5.26f, -1.27f, -0.0001f),
+                new Vector3
+(5.26f, -0.82f, -0.0001f),
+                new Vector3
+(3.45f, -0.47f, -0.0001f),
+                new Vector3
+(2.81f, 2.01f, -0.0001f),
+                new Vector3
+(2.04f, 2.5f, -0.0001f),
+                new Vector3
+(0.56f, 3.2f, -0.0001f),
+                new Vector3
+(2.16f, -0.13f, -0.0001f),
+                new Vector3
+(2.217f, -0.744f, -0.0001f),
+                new Vector3
+(0.439f, -0.156f, -0.0001f),
+                new Vector3
+(1.131f, 0.383f, -0.0001f),
+                new Vector3
+(0.42f, 1.629f, -0.0001f),
+                new Vector3
+(0.42f, 1.629f, -0.0001f),
+                new Vector3
+(0.42f, 1.629f, -0.0001f),
+                new Vector3
+(0.42f, 1.629f, -0.0001f),
+                new Vector3
+(0.42f, 1.629f, -0.0001f),
+                new Vector3
+(0.42f, 1.629f, -0.0001f),
+                new Vector3
+(-0.298f, 2.036f, -0.0001f),
+                new Vector3
+(-1.816f, 2.571f, -0.0001f),
+                new Vector3
+(-0.535f, 1.026f, -0.0001f),
+                new Vector3
+(-0.688f, 1.17f, -0.0001f),
+                new Vector3
+(-1.495f, 1.107f, -0.0001f),
+                new Vector3
+(-1.014f, 0.35f, -0.0001f),
+                new Vector3
+(-1.837f, -0.266f, -0.0001f),
+                new Vector3
+(-2.598f, -0.958f, -0.0001f),
+                new Vector3
+(-2.385f, -1.04f, -0.0001f),
+                new Vector3
+(-2.61f, -0.052f, -0.0001f),
+                new Vector3
+(-3.51f, -0.926f, -0.0001f),
+                new Vector3
+(-3.152f, 0.367f, -0.0001f),
+                new Vector3
+(-4.451f, 0.572f, -0.0001f),
+                new Vector3
+(-3.869f, -1.614f, -0.0001f),
+                new Vector3
+(-2.51f, -1.696f, -0.0001f),
+                new Vector3
+(-1.256f, -2.209f, -0.0001f),
+                new Vector3
+(-0.274f, -0.097f, -0.0001f),
+                
+                };
             PlayerLocation = xcoordinates[locationCode];
             print("Sign is at location " + locationCode);
             GetComponent<RectTransform>().localPosition = PlayerLocation;
