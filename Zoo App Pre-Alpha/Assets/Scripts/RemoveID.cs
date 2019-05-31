a2ef4d0fe4d66d9214c623cbe253f34c272ab4de
lcdamon <46969012+lcdamon@users.noreply.github.com>
2019-05-31 13:32:30 -0500

    gallery pt 2

---
+++ Zoo App Pre-Alpha/Assets/Scripts/RemoveID.cs
@@ -0,0 +1,215 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveID : MonoBehaviour
+{
+
+    public GameObject MacawID;
+    public void MacawIDControl()
+    {
+        MacawID.SetActive(false);
+    }
+
+    public GameObject CraneID;
+    public void CraneIDControl()
+    {
+        CraneID.SetActive(false);
+    }
+
+    public GameObject GibbonID;
+    public void GibbonIDControl()
+    {
+        GibbonID.SetActive(false);
+    }
+
+    public GameObject HogID;
+    public void HogIDControl()
+    {
+        HogID.SetActive(false);
+    }
+
+    public GameObject RhinoID;
+    public void RhinoIDControl()
+    {
+        RhinoID.SetActive(false);
+    }
+
+    public GameObject GiraffeID;
+    public void GiraffeIDControl()
+    {
+        GiraffeID.SetActive(false);
+
+    }
+
+    public GameObject MeerkatID;
+    public void MeerkatIDControl()
+    {
+        MeerkatID.SetActive(false);
+    }
+
+    public GameObject StorkID;
+    public void StorkIDControl()
+    {
+        StorkID.SetActive(false);
+    }
+
+    public GameObject CassowaryID;
+    public void CassowaryIDControl()
+    {
+        CassowaryID.SetActive(false);
+    }
+
+    public GameObject KangarooID;
+    public void KangarooIDControl()
+    {
+        KangarooID.SetActive(false);
+    }
+
+    public GameObject ElandID;
+    public void ElandIDControl()
+    {
+        ElandID.SetActive(false);
+    }
+
+    public GameObject BontebokID;
+    public void BontebokIDControl()
+    {
+        BontebokID.SetActive(false);
+    }
+
+    public GameObject CrownedID;
+    public void CrownedIDControl()
+    {
+        CrownedID.SetActive(false);
+    }
+
+    public GameObject OstrichID;
+    public void OstrichIDControl()
+    {
+        OstrichID.SetActive(false);
+    }
+
+    public GameObject SpringbokID;
+    public void SpringbokIDControl()
+    {
+        SpringbokID.SetActive(false);
+    }
+
+    public GameObject ZebraID;
+    public void ZebraIDControl()
+    {
+        ZebraID.SetActive(false);
+    }
+
+    public GameObject TortoiseID;
+    public void TortoiseIDControl()
+    {
+        TortoiseID.SetActive(false);
+    }
+
+    public GameObject TigerID;
+    public void TigerIDControl()
+    {
+        TigerID.SetActive(false);
+    }
+
+    public GameObject RedTortoiseID;
+    public void RedTortoiseIDControl()
+    {
+        RedTortoiseID.SetActive(false);
+    }
+
+    public GameObject PuduID;
+    public void PuduIDControl()
+    {
+        PuduID.SetActive(false);
+    }
+
+    public GameObject BearID;
+    public void BearIDControl()
+    {
+        BearID.SetActive(false);
+    }
+
+    public GameObject GuineaID;
+    public void GuineaIDControl()
+    {
+        GuineaID.SetActive(false);
+    }
+
+    public GameObject HornbillID;
+    public void HornbillIDControl()
+    {
+        HornbillID.SetActive(false);
+    }
+
+    public GameObject LemurID;
+    public void LemurIDControl()
+    {
+        LemurID.SetActive(false);
+    }
+
+    public GameObject DuikerID;
+    public void DuikerIDControl()
+    {
+        DuikerID.SetActive(false);
+    }
+
+    public GameObject RedLemurID;
+    public void RedLemurIDControl()
+    {
+        RedLemurID.SetActive(false);
+    }
+
+    public GameObject CougarID;
+    public void CougarIDControl()
+    {
+        CougarID.SetActive(false);
+    }
+
+    public GameObject MonkeyID;
+    public void MOnkeyControl()
+    {
+        MonkeyID.SetActive(false);
+    }
+
+    public GameObject PorcupineID;
+    public void PorcIDControl()
+    {
+        PorcupineID.SetActive(false);
+    }
+
+    public GameObject TapirID;
+    public void TapirIDControl()
+    {
+        TapirID.SetActive(false);
+    }
+
+    public GameObject ScreamerID;
+    public void ScreamerIDControl()
+    {
+        ScreamerID.SetActive(false);
+    }
+
+    public GameObject FlamingoID;
+    public void FlamingoIDControl()
+    {
+        FlamingoID.SetActive(false);
+    }
+
+    public GameObject LeopardID;
+    public void LeopardIDControl()
+    {
+        LeopardID.SetActive(false);
+    }
+
+    public GameObject PandaID;
+    public void PandaIDControl()
+    {
+        PandaID.SetActive(false);
+    }
+
+   
+}
+
