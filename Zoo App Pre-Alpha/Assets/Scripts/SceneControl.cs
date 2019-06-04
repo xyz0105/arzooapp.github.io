15da56d01ed20f597910adf10346d6609efe0215
lcdamon <46969012+lcdamon@users.noreply.github.com>
2019-06-04 14:02:57 -0500

    Acknowledgements

--- Zoo App Pre-Alpha/Assets/Scripts/SceneControl.cs
+++ Zoo App Pre-Alpha/Assets/Scripts/SceneControl.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class SceneControl : MonoBehaviour
 {
     public void ChangeToScene(int changeTheScene)
     {
         SceneManager.LoadScene(changeTheScene);
     }
+    public GameObject PhotoAcknowledgements; 
+    public void PhotoControl ()
+    {
+        PhotoAcknowledgements.SetActive(true);
+        
+    }
+
+
 }
 
