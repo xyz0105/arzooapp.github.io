3d1d9d44e4f7eee8bb984d5c7bfcda49ec041788
Garrett-Scott <47392469+Garrett-Scott@users.noreply.github.com>
2019-06-04 13:49:49 -0500

    android plugin
    also the arscene interface

--- Zoo App Pre-Alpha/Assets/Scripts/InactivityScript.cs
+++ Zoo App Pre-Alpha/Assets/Scripts/InactivityScript.cs
@@ -7,53 +7,70 @@ public class InactivityScript : MonoBehaviour
 {
     
     public CanvasGroup canvas;
-    bool running;
-    private int elapsed = 0;
-    private bool timeElapsed;
+    
+    
+    public bool bob;
+    
+
+   
     
     // Start is called before the first frame update
     void Start()
     {
         
-        print("the script runs");
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.touchCount > 0 && running == false) { StartCoroutine(CountSeconds(5)); StartCoroutine(FadeImage(false)); running = true; } else { running = false; }
-        if(timeElapsed == true) { StopCoroutine(CountSeconds(5)); timeElapsed = false; StartCoroutine(FadeImage(true)); }
-        
+        //touches = Input.touchCount;
+        if(Input.touchCount > 0 || bob == true) {  FadeImage(false); }
+       
     }
 
-    IEnumerator FadeImage(bool fadeAway)
+   public void FadeImage(bool fadeAway)
     {
         print("coroutine runs");
         // fade from opaque to transparent
         if (fadeAway)
         {
-            
+
+
+            StartCoroutine(wait());
             // loop over 1 second backwards
-            for (float i = 1; i >= 0; i -= Time.deltaTime)
-            {
-                // set color with i as alpha
-                canvas.alpha = i;
-                print("fading");
-                yield return null;
-            }
+            
         }
         // fade from transparent to opaque
         else
         {
+            
             // loop over 1 second
-            for (float i = 0; i <= 1; i += Time.deltaTime)
+            for (float i = 0; i <= 1; i += 0.01f)
             {
-                print("unfading");
+                
                 canvas.alpha = i;
-                yield return null;
+                
+                
+               
             }
         }
     }
-    IEnumerator CountSeconds(int seconds) { if (elapsed < seconds) { yield return new WaitForSeconds(1); elapsed += 1; } else { elapsed = 0; timeElapsed = true; } }
+    
+     IEnumerator wait() {
+        yield return new WaitForSecondsRealtime(0.5f);
+        for (float i = 1; i >= 0; i -= Time.deltaTime)
+            {
+                // set color with i as alpha
+               
+                
+                    canvas.alpha = i;
+
+            
+                    
+                   
+                
+                
+            }}
 }
